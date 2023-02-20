3420c6e1bb6aac6661da0ea5596c00b74cb06c6e
Morded <gab.jenei@gmail.com>
2023-02-20 12:51:51 +0100

    Testimonials mobil responsibility

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,17 +1,19 @@
+import Link from "next/link"
 
 type ReviewProps = {
   children: React.ReactNode
   by: string
+  link: string
 }
 
-const Review = ({ children, by }:ReviewProps ) => {
+const Review = ({ children, by, link }:ReviewProps ) => {
   return (
     <div className="flex flex-col justify-center text-center px-5 md:px-10 lg:px-20">
-      <div className="text-xl md:text-2xl leading-10 lg:leading-[3rem] tracking-tight">
+      <div className="text-base md:text-2xl leading-10 lg:leading-[3rem] tracking-tight font-bold">
         &ldquo;{children}&rdquo;
       </div>
       <div className="mt-6">
-        &mdash; {by}
+        &mdash; <Link target="_blank" href={link} className="text-sm">{by}</Link>
       </div>
     </div>
   )
--- src/components/Testimonals.tsx
+++ src/components/Testimonals.tsx
@@ -5,12 +5,12 @@ import testimonialList from "../../public/testimonials";
 
 const Testimonials = () => {
   return (
-    <Carousel className="bg-aocDark text-aocLight py-10 lg:py-20">
+    <Carousel className="bg-aocOrange text-aocDark py-10 lg:py-20">
       {testimonialList.map((testimonial, index) => {
         const { by, from, link, text } = testimonial;
         return (
           <CarouselItem key={index} index={index}>
-            <Review by={by +` (${from})`}>
+            <Review by={by +` (${from})`} link={link}>
               {text}
             </Review>
           </CarouselItem>
