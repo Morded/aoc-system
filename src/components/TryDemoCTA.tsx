2523f4df4ca566dd8b664d991ec52759f1260165
Morded <gab.jenei@gmail.com>
2023-02-20 20:31:27 +0100

    Added horizontal padding to the cta

--- src/components/TryDemoCTA.tsx
+++ src/components/TryDemoCTA.tsx
@@ -4,7 +4,7 @@ import SerifBody from "./SerifBody";
 
 const TryDemoCTA = () => {
   return (
-    <section className="w-full py-10 lg:py-20 bg-aocOrange bg-[url(/noise.png)] flex justify-center">
+    <section className="w-full px-4 lg:px-0 py-10 lg:py-20 bg-aocOrange bg-[url(/noise.png)] flex justify-center">
       <div className="max-w-7xl flex flex-col justify-center items-center text-center gap-8">
         <Header1>Próbálja ki ingyenes demóink egyikét</Header1>
         <SerifBody>
